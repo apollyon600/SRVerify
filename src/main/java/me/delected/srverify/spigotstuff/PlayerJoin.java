3dddb36289ec2b2d83604b951c81569508c1a00f
delected <58377193+Delected@users.noreply.github.com>
2020-11-07 19:01:52 -0600

    QOL

--- src/main/java/me/delected/srverify/spigotstuff/PlayerJoin.java
+++ src/main/java/me/delected/srverify/spigotstuff/PlayerJoin.java
@@ -6,20 +6,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class PlayerJoin implements Listener {
+    ServerBooleans s = new ServerBooleans();
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-        ServerBooleans s = new ServerBooleans();
         Player p = e.getPlayer();
         if (!s.isPlayerVerified(p)) {
             if (!s.isVerificationAllowed()) { p.kickPlayer(ChatColor.RED + "" + ChatColor.BOLD + "Sorry! \n" + ChatColor.DARK_GRAY + "The server is not accepting new testers right now."); }
             p.sendMessage(ChatColor.RED + "Welcome! In order to continue, you must verify. Use " + ChatColor.DARK_GRAY + "/verify" + ChatColor.RED + " to get started.");
-            // no need for flyspeed I think
-            p.setWalkSpeed(0);
             p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE, 10, true, false));
         }
     }
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) { if (!s.isPlayerVerified(e.getPlayer())) { e.setCancelled(true); } }
 }
